debbf1adc74d3063764a67d5caad28fbc9e2921a
danhayes <danhayes3008@outlook.com>
2020-04-19 14:17:04 +0100

    initial commit of the file

---
+++ js/boosts.js
@@ -0,0 +1,16 @@
+//player boosts
+var playerRank = 0.1;
+
+//research boosts
+
+//defence boosts
+var armour = 8;
+var shields = 8;
+//attack boosts
+var weapons = 7;
+var laser = 2;
+var ion = 3;
+var plasma = 2;
+
+document.write('<h2>' + 'Armour: ' + armour + '<h2>');
+console.log("Armour: " + armour);
